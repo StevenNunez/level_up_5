24dead428b583e8b5944e07b1ca9439dd16a176e
Steven Nunez <steven.nunez@gmail.com>
2015-06-17 20:46:07 -0400

    Add Controller

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,36 @@
-var app = angular.module('store', []);
+(function() {
+  var app = angular.module('store', []);
+
+  app.controller('StoreController', function(){
+    this.products = shirts;
+  })
+
+  var shirts = [
+      { shirtName: 'Casual Friday',
+          color: 'gray',
+          size: 'XL',
+          price: '$120',
+          owner: 'Steven'
+      },
+      { shirtName: 'I have your back',
+          color: 'orange',
+          size: 'L',
+          price: '$10',
+          owner: 'Steven'
+      },
+      { shirtName: 'Brother',
+          color: 'yellow',
+          size: 'L',
+          price: '$50',
+          owner: 'Andrew'
+      },
+      { shirtName: 'Save Tyrion',
+          color: 'black',
+          size: 'L',
+          price: '$50',
+          owner: 'Lucy'
+      }
+
+  ];
+
+})();
